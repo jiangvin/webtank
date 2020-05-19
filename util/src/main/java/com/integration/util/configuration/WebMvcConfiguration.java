e8540a8ba0a6b2a7fa8e50992808553807f7902f
Vin <wenlong.jiang@enmotech.com>
2020-05-19 11:36:51 +0800

    优化依赖包
    Change-Id: I702618c9b484302982cba9e5d548cfc158eb5d69

--- util/src/main/java/com/integration/util/configuration/WebMvcConfiguration.java
+++ util/src/main/java/com/integration/util/configuration/WebMvcConfiguration.java
@@ -1,4 +1,4 @@
-package com.integration.socket.configuration;
+package com.integration.util.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -6,7 +6,9 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.util.List;
 
@@ -18,6 +20,16 @@ import java.util.List;
 @Configuration
 @EnableWebMvc
 public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    /**
+     * 支持多语言，从url的lang中截取语言选择
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        localeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeInterceptor);
+    }
 
     /**
      * 降低string转换器的优先级，使string也可以顺利转换成resultData
