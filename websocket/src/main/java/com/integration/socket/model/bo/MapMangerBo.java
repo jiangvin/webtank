024bc977da4007d63208976efcef94f1bf207d86
Vin <wenlong.jiang@enmotech.com>
2020-06-04 09:09:55 +0800

    继承life属性
    Change-Id: I61ba8d6b537ee84da43cfd87a01521271332b868

--- websocket/src/main/java/com/integration/socket/model/bo/MapMangerBo.java
+++ websocket/src/main/java/com/integration/socket/model/bo/MapMangerBo.java
@@ -1,8 +1,12 @@
 package com.integration.socket.model.bo;
 
 import com.integration.dto.room.RoomType;
+import com.integration.socket.model.dto.StringCountDto;
 import com.integration.socket.service.MapService;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 蒋文龙(Vin)
@@ -25,14 +29,22 @@ public class MapMangerBo {
         this.mapService = mapService;
         this.roomType = roomType;
         this.mapBo = mapService.loadMap(mapId, roomType);
+        initPlayerLife();
         this.mapId = mapId;
     }
 
     public boolean loadNextMap() {
+        List<StringCountDto> lastPlayerLife = mapBo.getPlayerLife();
         MapBo mapBo = mapService.loadNextMap(++mapId, roomType);
         if (mapBo == null) {
             return false;
         }
+
+        //继承之前的属性
+        if (roomType == RoomType.PVE) {
+            mapBo.setPlayerLife(lastPlayerLife);
+        }
+
         this.mapBo = mapBo;
         return true;
     }
@@ -42,7 +54,22 @@ public class MapMangerBo {
         if (mapBo == null) {
             return false;
         }
+
         this.mapBo = mapBo;
+        initPlayerLife();
         return true;
+    }
+
+    private static List<StringCountDto> initPlayerLifeInPve() {
+        List<StringCountDto> list = new ArrayList<>();
+        list.add(new StringCountDto("tank01", 5));
+        return list;
+    }
+
+    private void initPlayerLife() {
+        if (roomType != RoomType.PVE) {
+            return;
+        }
+        this.mapBo.setPlayerLife(initPlayerLifeInPve());
     }
 }
