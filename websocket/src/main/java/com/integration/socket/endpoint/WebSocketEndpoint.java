a7f620aaddbc8ec89841e57ca1e668a0aa191cb4
Vin <wenlong.jiang@enmotech.com>
2020-06-24 15:10:33 +0800

    删除socket on error, 会吃掉系统异常
    Change-Id: I7c9f35638fd35e57a355222863082b4a2812a3b3

--- websocket/src/main/java/com/integration/socket/endpoint/WebSocketEndpoint.java
+++ websocket/src/main/java/com/integration/socket/endpoint/WebSocketEndpoint.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.OnClose;
-import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -88,10 +87,5 @@ public class WebSocketEndpoint {
             return;
         }
         gameService.receiveMessage(messageDto, userId);
-    }
-
-    @OnError
-    public void onError(Session session, Throwable error) {
-        log.warn("catch session:{} error:{}", session.getId(), error.getMessage());
     }
 }
