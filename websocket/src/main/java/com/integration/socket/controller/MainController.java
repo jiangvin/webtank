b21de32e1c0c3271f2a557438377c14830975f9a
Vin <wenlong.jiang@enmotech.com>
2020-12-11 10:19:26 +0800

    删除chat页面
    Change-Id: Ib5142d2eab571cb3ce34eae5ac3f9e1f54b90e7d

--- websocket/src/main/java/com/integration/socket/controller/MainController.java
+++ websocket/src/main/java/com/integration/socket/controller/MainController.java
@@ -27,14 +27,6 @@ public class MainController {
         return mav;
     }
 
-    @GetMapping("/chat")
-    public ModelAndView chat() {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("chat");
-        mav.getModel().put("name", "chat");
-        return mav;
-    }
-
     @GetMapping("/map")
     public ModelAndView map() {
         ModelAndView mav = new ModelAndView();
