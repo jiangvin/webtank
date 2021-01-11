2b306298c2bfcec51729f526c5070c37db21b800
Vin <wenlong.jiang@enmotech.com>
2021-01-11 17:33:05 +0800

    优化web loading页面
    Change-Id: I7e404adca24b7b7ecc37ee440701cb83a4ef6a7f

--- websocket/src/main/resources/static/js/web/loading.js
+++ websocket/src/main/resources/static/js/web/loading.js
@@ -12,10 +12,9 @@ export default class Loading extends Stage {
         super();
 
         this.percent = 0;
+        this.isInit = false;
 
-        const thisLoading = this;
-
-        //背景色图层
+        //background
         this.createItem({
             draw: function (ctx) {
                 ctx.fillStyle = '#01A7EC';
@@ -35,7 +34,7 @@ export default class Loading extends Stage {
 
         //progress bar
         this.createItem({
-            draw: function (ctx) {
+            draw: ctx => {
                 const width = Resource.formatWidth() * .4;
                 const height = 50;
                 const pos = {
@@ -57,7 +56,7 @@ export default class Loading extends Stage {
                 ctx.displayFillRoundRect(
                     pos.x,
                     pos.y,
-                    width * (thisLoading.percent / 100.0),
+                    width * (this.percent / 100.0),
                     height,
                     height / 2);
 
@@ -65,11 +64,10 @@ export default class Loading extends Stage {
                 ctx.textAlign = 'center';
                 ctx.textBaseline = 'top';
                 ctx.fillStyle = '#FFF';
-                ctx.displayText('资源已加载' + thisLoading.percent + '%',
+                ctx.displayText('资源已加载' + this.percent + '%',
                     pos.x + width / 2,
                     pos.y + 65,
                     36);
-
             }
         });
 
@@ -77,18 +75,13 @@ export default class Loading extends Stage {
     }
 
     initEvent() {
-        const thisLoading = this;
         const total = Resource.instance.images.size + Sound.instance.sounds.size;
         let loaded = 0;
 
-        const event = function () {
-            if (thisLoading.percent >= 100) {
-                return;
-            }
-
+        const event = () => {
             ++loaded;
-            thisLoading.percent = Math.floor(loaded * 100 / total);
-            if (loaded >= total) {
+            this.percent = Math.floor(loaded * 100 / total);
+            if (this.percent >= 100 && this.isInit) {
                 Resource.getRoot().nextStage();
             }
         };
@@ -127,10 +120,10 @@ export default class Loading extends Stage {
         };
 
         //切换至后台时静音
-        function handleVisibilityChange() {
+        const handleVisibilityChange = () => {
             if (document.hidden) {
                 //记录开始时间
-                thisLoading.startTime = new Date().getTime();
+                this.startTime = new Date().getTime();
                 createjs.Sound.volume = 0;
             } else {
                 createjs.Sound.volume = Sound.instance.volume;
@@ -138,31 +131,30 @@ export default class Loading extends Stage {
                 //检测时间，如果超过5分钟则重启
                 //TODO - 在安卓中会失效，暂无解决方案
                 const currentTime = new Date().getTime();
-                if (currentTime - thisLoading.startTime >= 5 * 60 * 1000) {
+                if (currentTime - this.startTime >= 5 * 60 * 1000) {
                     document.location.reload();
                 }
             }
-        }
+        };
         document.addEventListener("visibilitychange", handleVisibilityChange);
     }
 
     init() {
-        const thisLoading = this;
+        if (this.percent >= 100) {
+            Resource.getRoot().nextStage();
+            return;
+        }
+        this.isInit = true;
 
         //每5秒检测一次，若5秒进度条未更新则直接进入
-        let lastPercent = thisLoading.percent;
-        const checkPercent = function () {
-            if (thisLoading.percent >= 100) {
-                return;
-            }
-
-            if (thisLoading.percent > lastPercent) {
+        let lastPercent = this.percent;
+        const checkPercent = () => {
+            if (this.percent > lastPercent) {
                 //进度条已更新
-                lastPercent = thisLoading.percent;
+                lastPercent = this.percent;
                 setTimeout(checkPercent, 5000);
             } else {
                 //进度条未更新,直接进入
-                thisLoading.percent = 100;
                 Resource.getRoot().nextStage();
             }
         };
