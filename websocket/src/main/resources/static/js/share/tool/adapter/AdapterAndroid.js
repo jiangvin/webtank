8ee936b460f720c829f38845b08a85bb0ed7ba16
Vin <wenlong.jiang@enmotech.com>
2021-01-28 20:52:55 +0800

    优化adapter的初始化参数
    Change-Id: Idcbf2b123fe211ca844a3caa86200f6ec587724a

--- websocket/src/main/resources/static/js/share/tool/adapter/AdapterAndroid.js
+++ websocket/src/main/resources/static/js/share/tool/adapter/AdapterAndroid.js
@@ -1,5 +1,10 @@
 import Adapter from "./Adapter.js";
 import Sound from "../sound.js";
+import Control from "../control.js";
+import Common from "../common.js";
+import Resource from "../resource.js";
+import Loading from "../../../web/loading.js";
+import AppHome from "../../../app/apphome.js";
 
 /**
  * @author 蒋文龙(Vin)
@@ -11,6 +16,24 @@ export default class AdapterAndroid extends Adapter {
     constructor(mock) {
         super();
         this.mock = mock;
+    }
+
+    initGame(callback) {
+        Control.setControlMode(true);
+        Common.getRequest("/user/getUser?userId=" + Resource.getUser().deviceId, data => {
+            if (data) {
+                //旧用户
+                Resource.setUser(data);
+                Resource.getRoot().addStage(new Loading());
+                Resource.getRoot().addGameStage();
+            } else {
+                //新用户
+                Resource.getRoot().addStage(new Loading());
+                Resource.getRoot().addStage(new AppHome());
+                Resource.getRoot().addGameStage();
+            }
+            callback();
+        });
     }
 
     saveConf() {
